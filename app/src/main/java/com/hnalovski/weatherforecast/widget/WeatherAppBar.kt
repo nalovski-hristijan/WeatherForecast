553ed6f12aa48b14b0ab97ba5c19733307b0c713
Hristijan Nalovski <nalovski.hristijan@protonmail.com>
2024-07-01 15:51:25 +0200

    Added the drop down menu

--- app/src/main/java/com/hnalovski/weatherforecast/widget/WeatherAppBar.kt
+++ app/src/main/java/com/hnalovski/weatherforecast/widget/WeatherAppBar.kt
@@ -1,21 +1,38 @@
 package com.hnalovski.weatherforecast.widget
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -41,6 +58,14 @@ fun WeatherAppBar(
     onAddAction: () -> Unit = {},
     onButtonClicked: () -> Unit = {}
 ) {
+
+    val showDialog = remember {
+        mutableStateOf(false)
+    }
+
+    if (showDialog.value) {
+        ShowSettingDropDownMenu(showDialog = showDialog, navController = navController)
+    }
     CenterAlignedTopAppBar(
         title = {
             Text(
@@ -70,7 +95,9 @@ fun WeatherAppBar(
                     Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
                 }
 
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = {
+                    showDialog.value = true
+                }) {
                     Icon(
                         imageVector = Icons.Default.MoreVert, contentDescription = "More button"
                     )
@@ -82,4 +109,49 @@ fun WeatherAppBar(
                 }
             }
         })
+}
+
+@Composable
+fun ShowSettingDropDownMenu(showDialog: MutableState<Boolean>, navController: NavController) {
+
+    var expanded by remember {
+        mutableStateOf(true)
+    }
+
+    val items = listOf("About", "Favorites", "Settings")
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(align = Alignment.TopEnd)
+            .absolutePadding(top = 45.dp, right = 20.dp)
+    ) {
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(140.dp)
+                .background(Color.White)
+        ) {
+            items.forEachIndexed { index, text ->
+                DropdownMenuItem(text = {
+                    Text(text = text, modifier = Modifier.clickable {
+
+                    }, fontWeight = FontWeight.W300)
+                }, onClick = {
+                    expanded = false
+                    showDialog.value = false
+                }, leadingIcon = {
+                    Icon(
+                        imageVector = when (text) {
+                            "About" -> Icons.Default.Info
+                            "Favorites" -> Icons.Default.Favorite
+                            "Settings" -> Icons.Default.Settings
+                            else -> Icons.Default.Settings
+                        }, contentDescription = null, tint = Color.LightGray
+                    )
+                })
+            }
+        }
+    }
 }
